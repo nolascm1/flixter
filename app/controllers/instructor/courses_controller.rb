60dfc8dc2ad15719eda3ad726105397bb8ac7838
Melissa <nolascm1@gmail.com>
2019-07-29 01:28:55 +0000

    Lesson Permissions

--- app/controllers/instructor/courses_controller.rb
+++ app/controllers/instructor/courses_controller.rb
@@ -1,5 +1,6 @@
 class Instructor::CoursesController < ApplicationController
     before_action   :authenticate_user!
+    before_action   :require_authorized_for_current_course, only: [:show]
 
     def new
         @course = Course.new
@@ -19,7 +20,22 @@ class Instructor::CoursesController < ApplicationController
         end
     end
 
+    def show
+    end
+
+
     private
+
+    def require_authorized_for_current_course
+        if current_course.user != current_user
+            render plain: "Unauthorized", status: :Unauthorized
+        end
+    end
+
+    helper_method :current_course
+    def current_course
+        @current_course ||= Course.find(params[:id])
+    end
 
     def course_params
         params.require(:course).permit(:title, :description, :cost)
