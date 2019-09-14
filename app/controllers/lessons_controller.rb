4d24d8ba3881c9d99d9744462b30a276b2de3031
Melissa <nolascm1@gmail.com>
2019-09-14 20:14:58 +0000

    lesson video

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,11 +1,29 @@
 class LessonsController < ApplicationController
-    def show
         before_action :authenticate_user!
-    end
-private
+        before_action :require_authorized_for_current_section
 
-helper_method :current_lesson
-def current_lesson
-    @current_lesson ||= Lesson.find(params[:id])
+        def new
+            @lesson = Lesson.new
+        end
+
+        def create
+            @lesson = current_section.lessons.create(lesson_params)
+            redirect_to instructor_course_path(current_section.course)
+        end
+private
+        def require_authorized_for_current_section
+            if current_section.course.user != current_user
+                rend plain: 'Unauthorized', status: :unauthorized
+            end
+        end
+
+helper_method :current_section
+def current_section
+    @current_section ||= Section.find(params[:section_id])
+end
+
+def lesson_params
+    params.require(:lesson).permit(:title, :subtitle, :video)
 end
 end
+
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_17_222441) do
+ActiveRecord::Schema.define(version: 2019_09_14_195958) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 2019_08_17_222441) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "row_order"
+    t.string "video"
     t.index ["row_order"], name: "index_lessons_on_row_order"
     t.index ["section_id"], name: "index_lessons_on_section_id"
   end
