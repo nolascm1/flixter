eac74ed89df566bbf5b15b66f6bbb5096e46851d
Melissa <nolascm1@gmail.com>
2019-09-28 23:54:45 +0000

    modals

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,14 @@ class User < ApplicationRecord
   has_many  :courses
   has_many  :enrollments
   has_many  :enrolled_courses, through: :enrollments, source: :course
+  has_many  :images
+  has_many  :courses
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many  :images
-  has_many  :courses
 
   def enrolled_in?(course)
       return enrolled_courses.include?(course)
--- db/migrate/20190808151936_create_images.rb
+++ db/migrate/20190808151936_create_images.rb
@@ -6,7 +6,7 @@ class CreateImages < ActiveRecord::Migration[5.2]
       t.timestamps
     end
 
-    add_index :images, [:course_id]
-    add_index :images, :place_id
+    add_index :images, [:user_id, :course_id]
+    add_index :images, :course_id
   end
 end
