ec420ae4c72747ad3047cfc611a372e70ba39c6c
Melissa <nolascm1@gmail.com>
2019-10-20 21:41:20 +0000

    Post_Courses

--- config/routes.rb
+++ config/routes.rb
@@ -4,19 +4,18 @@ Rails.application.routes.draw do
     root 'static_pages#index'
     get 'privacy', to: 'static_pages#privacy'
     resources :courses, only: [:index, :show] do
-      resources :enrollments, only: :create
-      resources :images, only: :create
+    resources :enrollments, only: :create
+    resources :images, only: :create
     end
     resources :lessons, only: [:show]
     namespace :instructor do
+      resources :courses, only: [:new, :create, :show] 
+    end
       resources :lessons, only: [:update]
       resources :sections, only: [:update] do
-        resources :lessons, only: [:create]
-      end
-     resources :courses, only: [:new, :create, :show] do
+      resources :lessons, only: [:create]
       resources :sections, only: [:create]
       end
-     end
     end
 
   #resources :lessons, only: [:show]
