4ea775ec136b9573c37070381340840e97681c35
Melissa <nolascm1@gmail.com>
2019-10-21 19:24:10 +0000

    Resubmission Attempt

--- app/models/course.rb
+++ app/models/course.rb
@@ -3,7 +3,7 @@ class Course < ActiveRecord::Base
     belongs_to  :user
     has_many    :sections
     has_many    :enrollments
-    has_many    :images
+    
     validates :title, presence: true
     validates :description, presence: true
     validates :cost, presence: true, numericality: {greater_than_or_equal_to: 0}
